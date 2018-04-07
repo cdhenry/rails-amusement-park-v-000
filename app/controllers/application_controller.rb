d9f5ab41276c707ddd895ed21c7dadef4a7752e7
Chris Henry <cdhenry@gmail.com>
2018-04-07 23:13:12 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
-  include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    @user = (User.find_by(id: session[:user_id]) || User.new)
+  end
 
+  def logged_in?
+    current_user.id != nil
+  end
+
+  def require_logged_in
+    return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  resources :users, only: [:new, :create]
 
-
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
