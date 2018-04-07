946394e3d821e264202f47a887b6f6aa707c958d
Chris Henry <cdhenry@gmail.com>
2018-04-07 23:36:06 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  resources :users, only: [:new, :create]
+  resources :users
+  resources :attractions
 
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
