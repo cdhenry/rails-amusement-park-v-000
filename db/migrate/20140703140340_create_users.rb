4c78a57c6cdd474e13d34689c0b0ba31acab4a7d
Chris Henry <cdhenry@gmail.com>
2018-04-07 22:14:50 +0000

    Done.

--- db/migrate/20140703140340_create_users.rb
+++ db/migrate/20140703140340_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-      t.boolean :admin, default: false
       t.integer :tickets
       t.integer :nausea
       t.integer :happiness
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703140419) do
+ActiveRecord::Schema.define(version: 20180407221031) do
 
   create_table "attractions", force: :cascade do |t|
     t.string  "name"
@@ -29,11 +29,11 @@ ActiveRecord::Schema.define(version: 20140703140419) do
   create_table "users", force: :cascade do |t|
     t.string  "name"
     t.string  "password_digest"
-    t.boolean "admin",           default: false
     t.integer "tickets"
     t.integer "nausea"
     t.integer "happiness"
     t.integer "height"
+    t.boolean "admin",           default: false
   end
 
 end
