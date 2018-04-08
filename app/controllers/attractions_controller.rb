9b3928f53dbf97a7a61193342ce46924522fb0e1
Chris Henry <cdhenry@gmail.com>
2018-04-08 03:33:04 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -19,18 +19,21 @@ class AttractionsController < ApplicationController
   def create
     attraction = Attraction.new(attraction_params)
     if attraction.save
-      redirect_to user_path(attraction)
+      redirect_to attraction_path(attraction)
     else
       redirect_to :new
     end
   end
 
   def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attraction_params)
+    redirect_to attraction_path(@attraction)
   end
 
   private
 
-  def user_params
+  def attraction_params
     params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
   end
 end
